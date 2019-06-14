82b86454b183d0f82c95ae417c705aa24a476b49
Mateusz Bargiel <m.bargiel@avanade.com>
2019-06-14 13:47:38 +0200

    settings

--- env/backend.tf
+++ env/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "azurerm" {
     #example properties
-    #resource_group_name  = ""
-    #storage_account_name = ""
-    #container_name       = ""
-    #key                  = ""
-    #access_key           = ""
+    resource_group_name  = "avanade-terraform-rg"
+    storage_account_name = "avanadeterraformstorage"
+    container_name       = "terraform"
+    key                  = "eZI9R8pGmtIJpmj782zlnwHq/615SKHw7SI6T4tUOB1krCqvKMwAHMA5XvKBgWaVHhQXApfsSLYcOywmLB0QQQ=="
+    access_key           = "eZI9R8pGmtIJpmj782zlnwHq/615SKHw7SI6T4tUOB1krCqvKMwAHMA5XvKBgWaVHhQXApfsSLYcOywmLB0QQQ=="
   }
 }
