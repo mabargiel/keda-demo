20e130849b897feea4d85eb769047f6ad47a6916
Mateusz Bargiel <m.bargiel@avanade.com>
2019-07-21 15:38:08 +0200

    backend-changes

--- env/backend.tf
+++ env/backend.tf
@@ -1,10 +1,11 @@
 terraform {
   backend "azurerm" {
     #example properties
-    resource_group_name  = "avanade-terraform-rg"
-    storage_account_name = "avanadeterraformstorage"
-    container_name       = "terraform"
-    key                  = "eZI9R8pGmtIJpmj782zlnwHq/615SKHw7SI6T4tUOB1krCqvKMwAHMA5XvKBgWaVHhQXApfsSLYcOywmLB0QQQ=="
-    access_key           = "eZI9R8pGmtIJpmj782zlnwHq/615SKHw7SI6T4tUOB1krCqvKMwAHMA5XvKBgWaVHhQXApfsSLYcOywmLB0QQQ=="
+    resource_group_name  = "avanade-keda-demo-rg"
+    storage_account_name = "kedademostorage"
+    container_name       = "terraform-state"
+    key                  = "7zJGDm2vDOzuqcxzDxA7W8ylXqQeIy/eNe6UpXAS/WTJ1gQVKs8EkOeTJdEWqOGAycypiUvecqvTK2kWCsP68A=="
+    access_key           = "6wVceURjilmh99Ym4sOgeV19qm1arOd55JRUSQzwo6xpEbSbp6BjG5AbDX7BoraBGEMBPjsknoc/lOPW5H5AtQ=="
   }
 }
+
